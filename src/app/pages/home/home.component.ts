f5db1fbf4b38afa13d5c99b590cddd9973da4914
SebastianGilian <mgiliana1@upao.edu.pe>
2024-11-15 23:09:06 -0500

    fix: Redireccion de registro

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarHomeComponent } from '../../shared/components/navbar-home/navbar-home.component';
 import { FooterComponent } from '../../shared/components/footer/footer.component';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarHomeComponent, FooterComponent],
+  imports: [NavbarHomeComponent, FooterComponent, RouterLink, RouterOutlet],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
