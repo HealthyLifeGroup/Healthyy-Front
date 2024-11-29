3721331bafabec92fcd27d19e508937efeca0c00
RodrigoA <ragredai1@upao.edu.pe>
2024-11-28 23:27:03 -0500

    fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    baseURL: 'https://healthy-api-latest-voxt.onrender.com/api/v1',
+    baseURL: 'https://healthy-api-latest-h3le.onrender.com/api/v1',
     paypalUrl: 'https://healthy-web-v2.netlify.app/customer/subscription'
 };
