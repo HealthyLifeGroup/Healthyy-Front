d82cc62c500d83ff0d9679d36345624bdf180720
RodrigoA <ragredai1@upao.edu.pe>
2024-11-28 21:55:36 -0500

    Cambio de url de paypal

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
     baseURL: 'https://healthy-api-latest-q67s.onrender.com/api/v1',
-    paypalUrl: 'http://localhost:4200/customer/subscription'
+    paypalUrl: 'https://healthy-web-v2.netlify.app/customer/subscription'
 };
